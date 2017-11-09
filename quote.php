7dbd9a3eb3c5f2ab0da713e218beeb66b729f1cf
zxzcafzvsx <31308588+zxzcafzvsx@users.noreply.github.com>
2017-11-09 19:52:50 +0700

    Create quote.php

---
+++ quote.php
@@ -0,0 +1,26 @@
+<?php
+function rand_line($fileName, $maxLineLength = 4096) {
+    $handle = @fopen($fileName, "r");
+    if ($handle) {
+        $random_line = null;
+        $line = null;
+        $count = 0;
+        while (($line = fgets($handle, $maxLineLength)) !== false) {
+            $count++;
+            // P(1/$count) probability of picking current line as random line
+            if(rand() % $count == 0) {
+              $random_line = $line;
+            }
+        }
+        if (!feof($handle)) {
+            echo "Error: unexpected fgets() fail\n";
+            fclose($handle);
+            return null;
+        } else {
+            fclose($handle);
+        }
+        return $random_line;
+    }
+}
+echo rand_line("quote.txt");
+?>
